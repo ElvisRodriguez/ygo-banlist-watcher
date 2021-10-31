63c3573af8a6a7c0eb2db9908035771c6753991a
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-10-31 15:27:52 -0400

    Created basic structure for emailer, need to implement proper authentication for secure use

---
+++ api/send_email.py
@@ -0,0 +1,36 @@
+import smtplib
+import ssl
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from dotenv import dotenv_values
+
+from path_resolver import get_full_path
+
+CONFIG = dotenv_values(get_full_path(__file__, ".env.secret"))
+
+def send_email(recepient):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Testing HTML"
+    message["From"] = CONFIG["CRON_EMAIL"]
+    message["To"] = recepient
+    text = "Plain Text Message Test"
+    html="""
+    <html>
+        <body>
+            <p>HTML Message Test</p>
+        </body>
+    </html>
+    """
+    message.attach(MIMEText(text, "plain"))
+    message.attach(MIMEText(html, "html"))
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(
+        "smtp.gmail.com", CONFIG["SSL_PORT"], context=context) as server:
+        server.login(CONFIG["CRON_EMAIL"], CONFIG["CRON_PASSWORD"])
+        server.sendmail(CONFIG["CRON_EMAIL"], recepient, message.as_string())
+
+
+if __name__ == "__main__":
+    send_email("elvisrodriguez1992@gmail.com")
