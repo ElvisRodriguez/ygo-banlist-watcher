4ac69c0c51ad46c6ddaed0e838b0a59300c69417
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2023-02-06 14:48:02 -0500

    reworked scraper to work with hashing stringified scraped data

--- api/scraper.py
+++ api/scraper.py
@@ -1,38 +1,34 @@
 from bs4 import BeautifulSoup
 from dotenv import dotenv_values
+import hashlib
 import requests
 
 from path_resolver import get_full_path
 
+
 CONFIG = dotenv_values(get_full_path(__file__, ".env"))
 
-class BanlistScraper(object):
-    def __init__(self):
-        response = requests.get(CONFIG["URL"])
-        self.soup = BeautifulSoup(response.content, features="lxml")
-    
-    def get_update_text(self):
-        """Scrape the text containing the date of the last banlist update.
 
-        Args:
-            None.
-        Returns:
-            update_text(str): text of the form "UPDATED: MM/DD/YYYY".
-        """
-        raw_update_date = self.soup.select(".update_ttl")
-        update_text = raw_update_date[0].text 
-        return update_text
-    
-    def get_card_data(self):
-        """Scrape all card rows from current Yugioh F&L list tables.
+def get_banlist_data():
+    response = requests.get(CONFIG["URL"])
+    soup = BeautifulSoup(response.content, features="html.parser")
+    javascript_tags = soup.select("script")
+    banlist_tag, *_ = [tag for tag in javascript_tags if "Forbidden" in str(tag)]
+    banlist_data = str(banlist_tag)
+    return banlist_data
 
-        Args:
-            None.
-        Returns:
-            cards (list iterator): All <tr> tags scraped from banlist page.
-        """
-        table_rows = [
-            table_row for table_row in self.soup.select("[class^='cardlist']")]
-        cards = list(filter(
-            lambda tr: "cardlist_atitle" not in tr.attrs["class"], table_rows))
-        return cards
+
+def check_previous_banlist_data(banlist_data):
+    hash_data = hashlib.md5(banlist_data.encode())
+    with open(get_full_path(__file__, "hashed_banlist_data.txt"), 'r') as file:
+        previous_hash_data = file.readline().strip('\n')
+        if hash_data.hexdigest() != previous_hash_data:
+            print("Banlist Changed!")
+            with open(get_full_path(__file__, "hashed_banlist_data.txt"), 'w') as file:
+                file.write(hash_data.hexdigest())
+
+
+if __name__ == "__main__":
+    print("Checking banlist...")
+    banlist_data = get_banlist_data()
+    check_previous_banlist_data(banlist_data)
